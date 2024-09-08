4c0c43f7d1971a030e13d290df616889b88dbf0d
roy0424 <roy0424@naver.com>
2024-09-08 19:47:30 +0900

    add mongo connection test

---
+++ authentication/src/test/kotlin/com/kohan/authentication/util/MongoDbConnectionTest.kt
@@ -0,0 +1,21 @@
+package com.kohan.authentication.util
+
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.data.mongodb.core.MongoTemplate
+
+@SpringBootTest
+class MongoDbConnectionTest {
+
+    @Autowired
+    lateinit var mongoTemplate: MongoTemplate
+
+    @Test
+    fun testMongoDbConnection() {
+        // MongoDB 연결 상태 확인
+        val collectionNames = mongoTemplate.db.listCollectionNames().toList()
+        assertTrue(collectionNames.isNotEmpty(), "MongoDB에 연결할 수 없습니다.")
+    }
+}
