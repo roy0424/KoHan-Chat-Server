243e3580f314ae561b09fcbcd45e7b1751ba5dec
roy0424 <roy0424@naver.com>
2024-09-13 21:33:41 +0900

    add firebase config

---
+++ push/src/main/kotlin/com/kohan/push/firebase/FirebaseConfig.kt
@@ -0,0 +1,24 @@
+package com.kohan.push.firebase
+
+import com.google.auth.oauth2.GoogleCredentials
+import com.google.firebase.FirebaseApp
+import com.google.firebase.FirebaseOptions
+import java.io.FileInputStream
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class FirebaseConfig(
+    @Value("\${kohan.push.firebase.secret.key.path}")
+    private val firebaseConfigPath: String
+) {
+    @Bean
+    fun firebaseApp(): FirebaseApp {
+        val serviceAccount = FileInputStream(firebaseConfigPath)
+        val options = FirebaseOptions.builder()
+            .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+            .build()
+        return FirebaseApp.initializeApp(options)
+    }
+}
