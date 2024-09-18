7b163aac79ab435f97fecb1d870cf5cb1d4469fe
roy0424 <roy0424@naver.com>
2024-09-18 13:29:20 +0900

    remove component scan

--- authentication/src/main/kotlin/com/kohan/authentication/AuthenticationApplication.kt
+++ authentication/src/main/kotlin/com/kohan/authentication/AuthenticationApplication.kt
@@ -1,15 +1,14 @@
 package com.kohan.authentication
 
+import com.kohan.shared.spring.config.PushGrpcClient
 import com.kohan.shared.spring.mongo.config.EmbeddedMongoConfig
 import com.kohan.shared.spring.mongo.config.MongoConfig
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Import
 
-@Import(MongoConfig::class, EmbeddedMongoConfig::class)
+@Import(MongoConfig::class, EmbeddedMongoConfig::class, PushGrpcClient::class)
 @SpringBootApplication
-@ComponentScan(basePackages = ["com.kohan.authentication", "com.kohan.shared.spring.config"])
 class AuthenticationApplication
 
 fun main(args: Array<String>) {
