692c370d6c1cdaa97ab278d4a44db933a292e243
eunho ko <eunho.dev@gmail.com>
2024-09-18 16:38:18 +0900

    Lint Fix

--- file/src/main/kotlin/com/kohan/file/FileApplication.kt
+++ file/src/main/kotlin/com/kohan/file/FileApplication.kt
@@ -4,7 +4,6 @@ import com.kohan.shared.spring.mongo.config.EmbeddedMongoConfig
 import com.kohan.shared.spring.mongo.config.MongoConfig
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Import
 
 @Import(MongoConfig::class, EmbeddedMongoConfig::class)
